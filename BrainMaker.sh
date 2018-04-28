e9c5d70c9a4ae496717770685ffbf0de94913e57
JasonKChow <jaso.chow@mail.utoronto.ca>
2018-04-27 20:51:23 -0400

    New docker directories and positional arguments

--- BrainMaker.sh
+++ BrainMaker.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-cd Desktop/BrainMaker/
-
-export FREESURFER_HOME=/Applications/freesurfer
+export FREESURFER_HOME=freesurfer
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
-mris_convert lh.pial lh.pial.stl
-mris_convert rh.pial rh.pial.stl
+mris_convert $1 lh.pial.stl
+mris_convert $2 rh.pial.stl
 
-/Applications/blender-2.79-macOS-10.6/blender.app/Contents/MacOS/blender \
---background --python BrainMaker.py
+/blender/blender --background --python BrainMaker.py
 
-/Applications/Slic3r.app/Contents/MacOS/slic3r PrintBrain.stl \
+/slic3r/bin/slic3r PrintBrain.stl \
 --load BrainMaker.ini --scale 0.2 --layer-height 0.2 --fill-density 0% \
 --support-material-pattern honeycomb --raft-layers 2 \
 --support-material-buildplate-only yes
