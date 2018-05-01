af0f751a46e7edbc743528134efb6babed874959
JasonKChow <jaso.chow@mail.utoronto.ca>
2018-05-01 18:54:15 -0400

    Added BIDS-App argument parsing

--- BrainMaker.sh
+++ BrainMaker.sh
@@ -2,6 +2,39 @@
 export FREESURFER_HOME=/usr/local/freesurfer
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
+# Named arguments
+participant_label=
+
+# Positional arguments
+bids_dir=
+output_dir=
+level=
+
+# Assign arguments
+while [ "$1" != "" ]; do
+  case $1 in
+    --participant_label )
+      shift
+      participant_label=$1
+      ;;
+    * )
+      if [ -z $bids_dir ]
+      then
+        bids_dir=$1
+      elif [ -z $output_dir ]
+      then
+        output_dir=$1
+      elif [ -z $level ]
+      then
+        level=$1
+      else
+        exit
+      fi
+  esac
+  shift
+done
+
+
 mris_convert $1 lh.pial.stl
 mris_convert $2 rh.pial.stl
 
