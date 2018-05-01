eec32223231760a3d2dc41d8278d146a029cb631
JasonKChow <jaso.chow@mail.utoronto.ca>
2018-05-01 12:14:05 -0400

    Removed some debugging comments.

--- BrainMaker.sh
+++ BrainMaker.sh
@@ -2,10 +2,10 @@
 export FREESURFER_HOME=/usr/local/freesurfer
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
-# mris_convert $1 lh.pial.stl
-# mris_convert $2 rh.pial.stl
+mris_convert $1 lh.pial.stl
+mris_convert $2 rh.pial.stl
 
-# /usr/local/blender/blender --background -noaudio -Y --python usr/local/BrainMaker.py
+/usr/local/blender/blender --background -noaudio -Y --python usr/local/BrainMaker.py
 
 /usr/local/slic3r/slic3r /files/PrintBrain.stl \
   --load /usr/local/BrainMaker.ini --scale 0.2 --layer-height 0.2 --fill-density 0% \
